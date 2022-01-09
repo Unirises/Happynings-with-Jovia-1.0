e35d235a1feb8e6d8f3d2d35ad4712efbe726ad9
Justin Pascual <chrono.social@outlook.com>
2022-01-09 21:50:16 +0800

    Initialize files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-0" : "#FCD14E",
+        "primary-1" : "#FDF1BF",
+        "primary-2" : "#FAC832",
+        "color-1": "#3D7E81",
+        "color-2": "#F39342"
+      },
+      fontFamily: {
+        "primary-font": "'Gochi Hand', cursive;",
+        "secondary-font": "'Questrial', sans-serif",
+        "tertiary-font" : "'Sniglet', cursive"
+      }
+    },
   },
   plugins: [],
 }
