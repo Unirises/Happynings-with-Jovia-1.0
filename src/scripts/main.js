f2535074e32442b9428c1bbe1b6534086aee2e6c
Justin Pascual <chrono.social@outlook.com>
2022-01-10 22:15:36 +0800

    files optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,60 @@
-  if (sessionStorage.getItem('page_id') == null) {
-      sessionStorage.setItem('page_id', 0)
-  }
+if (sessionStorage.getItem('page_id') == null) {
+    sessionStorage.setItem('page_id', 0)
+}
 
-  const app = new Vue({
-      el: '#app',
-      data: {
-          active_page: 0,
-          user_answer: ''
-      },
-      methods: {
-          nextPage: function (current_page) {
-              sessionStorage.setItem('page_id', current_page);
-              this.active_page = current_page
-          },
-          getPage: function () {
-              this.active_page = parseInt(sessionStorage.getItem('page_id'))
-              return this.active_page
-          },
-          checkAnswer: function (answer, page_id) {
-              if (answer === btoa(this.user_answer.toUpperCase())) {
-                  this.nextPage(page_id)
-                  this.user_answer = ''
-              }
+function getImageUrl(name) {
+  return new URL(`../images/${name}.png`, import.meta.url).href
+}
 
-              const answer_field = document.querySelector('#answer_field_1');
-              answer_field.classList.replace('border-primary-2', 'border-red-500')
-              answer_field.classList.add('animate-pulse')
-              setTimeout(() => {
-                  answer_field.classList.remove('animate-pulse')
-                  answer_field.classList.replace('border-red-500', 'border-primary-2')
-              }, 1000)
-          }
-      }
-  })
+const app = new Vue({
+    el: '#app',
+    data: {
+        active_page: 0,
+        user_answer: '',
+        sound_status: false,
+        answer_1: ['SElSQVlBIE1BTkFXQVJJ', 'SElSQU1BWUFOQQ=='],
+        answer_2: ['Wm9yYXlkYQ==', 'TWF0dXRpbmE='],
+        answer_3: ['QW5nIFBhZ29uZyBhdCBhbmcgTWF0c2luZyBieSBKUCBSaXphbA==', 'RmxvcmFudGUgYXQgTGF1cmE=', 'YW5nIFBhZ29uZyBhdCBhbmcgTWF0c2luZw==', 'UGFnb25nIGF0IE1hdHNpbmc='],
+        answer_4: ['aG95IGdpc2luZw==', 'TWFnYW5kYW5nIEdhYmkgQmF5YW4='],
+        answer_5: ['eWVsbG93']
+    },
+    methods: {
+        nextPage: function (current_page) {
+            sessionStorage.setItem('page_id', current_page);
+            this.active_page = current_page
+        },
+        getPage: function () {
+            this.active_page = parseInt(sessionStorage.getItem('page_id'))
+            return this.active_page
+        },
+        checkAnswer: function (answers, page_id) {
+            answers.forEach(answer => {
+                if (atob(answer).toUpperCase() == this.user_answer.toUpperCase()) {
+                    this.nextPage(page_id)
+                    this.user_answer = ''
+                }
+
+                const answer_field = document.querySelector('#answer_field');
+                answer_field.classList.replace('border-primary-2', 'border-red-500')
+                answer_field.classList.add('animate-pulse')
+                setTimeout(() => {
+                    answer_field.classList.remove('animate-pulse')
+                    answer_field.classList.replace('border-red-500', 'border-primary-2')
+                }, 1000)
+            });
+        },
+        soundStatus: function () {
+            this.sound_status = !this.sound_status;
+            const music = document.querySelector('#music');
+            const sound_status = document.querySelector('#soundStatus')
+
+            if (this.sound_status) {
+                music.play()
+                sound_status.src = getImageUrl('sound-on')
+            } else {
+                music.pause()
+                sound_status.src = getImageUrl('sound-off')
+            }
+        }
+    }
+})
